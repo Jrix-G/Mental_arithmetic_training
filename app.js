305c56040fd306457dc0fdccfc354dd4eb6e73e3
Jrix <jrixweb@gmail.com>
2023-09-03 01:42:06 +0200

    Adding structural/css/beauty

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 function number() {
     var nombre1 = document.getElementById("TextInput").value;
     var nombre2 = document.getElementById("TextInput2").value;
-    nombre_random_1 = Math.floor((Math.random() * nombre1));
-    nombre_random_2 = Math.floor((Math.random() * nombre2));
+    nombre_random_1 = Math.floor((Math.random() * nombre1+1));
+    nombre_random_2 = Math.floor((Math.random() * nombre2+1));
 
-    if (nombre_random_1 >= 0 && nombre_random_2 >= 0 && nombre1 != "" && nombre2 != ""){
-        document.getElementById("calcul").innerHTML = "What is the result of: " + nombre_random_1 + " multiplied by: " + nombre_random_2;
+    if (nombre_random_1 == 0){
+        nombre_random_1 = 1
+    }
+    if (nombre_random_2 == 0){
+        nombre_random_2 = 1
+    }
+
+    if (nombre_random_1 > 0 && nombre_random_2 > 0 && nombre1 != "" && nombre2 != ""){
+        document.getElementById("calcul").innerHTML = "What is the result of: " + nombre_random_1 + "*" + nombre_random_2;
     }else{
         document.getElementById("calcul").innerHTML = "First or second number must be greater or equal to 0.";
     };
@@ -31,7 +38,13 @@ function resultButton() {
     if (resultUser != result) {
         document.getElementById("calcul").innerHTML = "Not the right answer, try again!";
     } else {
-        document.getElementById("calcul").innerHTML = "Good!";
+        document.getElementById("calcul").innerHTML = "Good! Next in 2 second";
+        setTimeout(function(){
+            document.getElementById("calcul").innerHTML = "Go";
+        }, 1000);
+        setTimeout(function(){
+            number()
+        }, 2000);
         var x = document.getElementById("resultDiv");
         x.style.display = "none";
     };
